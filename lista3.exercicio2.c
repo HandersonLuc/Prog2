8ff6d9797f608751ca15532b731db7f710afa377
Handerson Lucena <160972288+HandersonLuc@users.noreply.github.com>
2025-04-14 19:49:07 -0300

    Feat: add exer2

---
+++ lista3.exercicio2.c
@@ -0,0 +1,10 @@
+/*Seja o seguinte trecho de programa:
+int i=3,j=5;
+int *p, *q;
+p = &i;
+q = &j;
+Qual é o valor das seguintes expressões ?
+a) p == &i; b) *p - *q c) **&p d) 3* - *p/(*q)+7*/
+
+#include <stdio.h>
+#include <stdlib.h>
