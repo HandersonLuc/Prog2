33401e4015c93ec45f246e13ba1f43213fea7ba2
Handerson Lucena <160972288+HandersonLuc@users.noreply.github.com>
2025-04-16 10:04:36 -0300

    Feat: mod

--- lista3.exercicio2.c
+++ lista3.exercicio2.c
@@ -8,3 +8,9 @@ a) p == &i; b) *p - *q c) **&p d) 3* - *p/(*q)+7*/
 
 #include <stdio.h>
 #include <stdlib.h>
+int main(){
+    printf("Na a o valor expresso True, pois p armagena o enderco de i, logo p == &i\n");
+    printf("Na b o valor expresso -2, pois *p = 3 e *q = 5, logo 3 - 5 = -2\n");
+    printf("Na c o valor expresso 3, pois **&p = *p = 3\n");
+    printf("Na d o valor expresso 3, pois 3 * -3 / (5) + 7 = 6, pois 3 veze -3 e igual a -9 que divide por 5 entrega -1, samado com 7 e 6.\n");
+}
