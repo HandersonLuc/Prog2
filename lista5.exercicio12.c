ff4fac652597599c88b5d30071b31913e6cf4992
Handerson Lucena <160972288+HandersonLuc@users.noreply.github.com>
2025-05-19 13:12:11 -0300

    rename

--- lista5.exercicio12.c
+++ lista5.exercicio12.c
@@ -8,7 +8,20 @@ estatística mostrada será 'O' = 2, ' '=3, 'E' = 3, 'X' = 1, 'R' = 1, 'C' = 3, 
 #include <stdio.h>
 
 int main(){
-    
-
+    char str[MAX];
+    int cont[256] = {0}; // Inicializa o vetor de contagem com 0
+    printf("Digite uma string (maximo 50 caracteres): ");
+    fgets(str, sizeof(str), stdin); // Lê a string do usuário
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != '\r'){
+            cont[(unsigned char)str[i]]++; // Incrementa a contagem do caractere
+        }
+    }
+    printf("Estatistica dos caracteres:\n");
+    for (int i = 0; i < 256; i++) {
+        if (cont[i] > 0) { // Verifica se o caractere apareceu
+            printf("'%c' = %d\n", i, cont[i]); // Imprime o caractere e sua contagem
+        }
+    }
     return 0;
 }
